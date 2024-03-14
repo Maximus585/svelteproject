7f296d78749032dc62341b3fcfdfbb43fd70b5c6
Maksim <maksim@mail.ru>
2024-03-14 11:59:18 +0500

    add blog

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,15 @@
+
+
+export const routes = [
+    {
+        id: 1,
+        title: 'My First Blog Post',
+        body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officia eaque nisi! Perspiciatis, molestiae.'
+    },
+    {
+        id: 2,
+        title: 'My Second Blog Post',
+        body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem officia eaque nisi! Perspiciatis, molestiae.'
+    }
+];
+
